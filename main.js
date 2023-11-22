65c704b109733769587fee855e825f0445ff61f5
Екатерина <96071466+Lapikova09@users.noreply.github.com>
2023-11-22 18:38:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+/*const name = prompt('Как тебя зовут?');
+alert(`Привет, ${name} !`);*/
+
+/*const year = Number(prompt('В каком году вы родились?'));
+const currYear = 2023;
+alert(currYear - year);*/
+
+/*const side = Number(prompt('Длина стороны квадрата равна..'));
+alert('Периметр равен ' + 4*side); */
+
+/*const radius = Number(prompt('Радиус окружности равен..'));
+alert('Площадь окружности равна ' + radius ** 2 * 3.14);*/
+
+/*const s = Number(prompt('Расстояние равно ... км'));
+const t = Number(prompt('Желательное время равно ... ч'));
+alert(`Вам нужно двигаться со скоростью ${s/t} км/ч`);*/
+
+/*const dollar = Number(prompt('Введите сумму в долларах'));
+const conv = 0.916;
+alert(`Это равно ${dollar*conv} евро`);*/
+
+/*const v = Number(prompt('Введите объем флешки в Гб'));
+const file = 820;
+alert(`Поместится ${v*1024/820 | 0} целых файла/ов`);*/
+
+/*const money = Number(prompt('Сколько у вас рублей?'));
+const chocolate = Number(prompt('Сколько стоит шоколадка?'));
+alert(`Вы можете купить ${money/chocolate | 0} шоколадки(ок), у вас останется ${money-(money/chocolate | 0)*chocolate} рублей`);*/
+
+/*const number = Number(prompt('Введите трехзначное число'));
+alert(number%10 * 100 + (number/10 | 0) % 10 * 10 + (number/100 | 0) % 10);*/
+
+/*const number = Number(prompt('Введите целое число'));
+function checkNum(num){
+    const a =['четное', 'нечетное']
+    return a[num % 2]
+}
+alert(checkNum(number));*/
